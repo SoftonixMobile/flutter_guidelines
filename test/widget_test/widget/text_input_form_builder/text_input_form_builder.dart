553511477ef7c2d0c002f65d0d7fd98e3716023d
Kateryna Kalinchenko <kate.kalinchenko@gmail.com>
2023-09-11 10:50:31 +0300

    Added tests for text input form builder

--- test/widget_test/widget/text_input_form_builder/text_input_form_builder.dart
+++ test/widget_test/widget/text_input_form_builder/text_input_form_builder.dart
@@ -1,1 +1,53 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_guidelines/localization/index.dart';
+import 'package:flutter_guidelines/widgets/form_builder/index.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stx_flutter_form_bloc/stx_flutter_form_bloc.dart';
+
+Future<void> main() async {
+  /// Init EasyLocalization.
+  SharedPreferences.setMockInitialValues({});
+  await EasyLocalization.ensureInitialized();
+
+  /// Call ensureInitialized, because main is async.
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  patrolWidgetTest(
+    'Text field error',
+    ($) async {
+      /// Mock bloc interactions.
+      final bloc = TextFieldBloc(
+        required: true,
+        rules: {ValidationType.onChange},
+        customValidators: {FieldBlocValidators.passwordMin6Chars},
+      );
+
+      /// Get the widget.
+      await $.pumpWidget(
+        EasyLocalization(
+          path: CodegenLoader.path,
+          supportedLocales: CodegenLoader.supportedLocales,
+          fallbackLocale: CodegenLoader.supportedLocales.last,
+          assetLoader: const CodegenLoader(),
+          child: MaterialApp(
+            home: Material(
+              child: TextInputFormBuilder(fieldBloc: bloc),
+            ),
+          ),
+        ),
+      );
+
+      /// Test if needed elements are present.
+      final textField = $(TextField);
+      expect(textField, findsOneWidget);
+
+      await textField.enterText('12');
+      await $.pump();
+
+      final error = $('Password length must be at least 6 characters');
+      expect(error, findsOneWidget);
+    },
+  );
+}
