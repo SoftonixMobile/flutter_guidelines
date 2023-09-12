a65987dacbda7aa1b6aee0697297cdea2392ed87
Kateryna Kalinchenko <kate.kalinchenko@gmail.com>
2023-09-12 11:17:48 +0300

    Removed extra test

--- integration_test/home/home_screen_test.dart
+++ integration_test/home/home_screen_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_guidelines/app.dart';
 import 'package:flutter_guidelines/app_initialization.dart';
 import 'package:flutter_guidelines/localization/index.dart';
 import 'package:flutter_guidelines/screens/home/dashboard/dashboard_screen.dart';
-import 'package:flutter_guidelines/screens/login/widgets/index.dart';
 import 'package:flutter_guidelines/services/index.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -71,34 +70,5 @@ Future<void> main() async {
 
     // If we don't have English button, language is changed.
     expect(language, findsNothing);
-  });
-
-  patrolWidgetTest('Logout', ($) async {
-    // Load auth screen.
-    await $.pumpWidget(
-      EasyLocalization(
-        path: CodegenLoader.path,
-        supportedLocales: CodegenLoader.supportedLocales,
-        fallbackLocale: CodegenLoader.supportedLocales.last,
-        assetLoader: const CodegenLoader(),
-        child: SoftonixApp(),
-      ),
-    );
-
-    // Wait couple of seconds till splash is removed.
-    await $.pumpAndSettle();
-
-    // Open drawer.
-    final drawer = $(Scaffold).$(Icon);
-    await drawer.tap();
-    await $.pump();
-
-    // Trigger logout.
-    final logout = $(ListTile).last;
-    await logout.tap();
-    await $.pump();
-
-    // We land on Home screen, dashboard tab.
-    expect($(LoginForm), findsOneWidget);
   });
 }
